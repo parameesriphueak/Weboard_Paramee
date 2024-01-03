a81b4b5c00b2c15da92cc49255a3544513f6b8db
parameesriphueak <s6603052421038@email.kmutnb.ac.th>
2024-01-03 15:57:26 +0700

    work_4
    work_4

---
+++ post.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>เข้าสู่ระบบ</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard Paramee</h1><hr><br>
+    <div style="text-align: center;">
+        ต้องการดูกระทู้หมายเลข  <?php echo $_GET['id']; ?><br>
+    </div>
+    <br>
+    <table style="border: 2px solid black; width: 40%;" align="center">
+        <tr><td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น </td></tr>
+        <form>
+            <tr><th colspan="2"><textarea rows="4" cols="70"></textarea></th></tr>
+            <tr><th colspan="2"><input type="submit" value="ส่งข้อความ" ></th></tr>
+        </form>
+        
+    </table>
+    <p style="text-align: center;"> <a href="index.html">กลับไปหน้าหลัก</a></p>
+
+    
+
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+
+    <h1 style="text-align: center;">Webboard Paramee</h1><hr><br>
+    <div style="text-align: center;">
+        เข้าสู่ระบบด้วย<br>
+        Login = <?php echo $_POST['username']; ?><br>
+        Password = <?php echo $_POST['password']; ?><br>
+    </div>
+
+</body>
+</html>
