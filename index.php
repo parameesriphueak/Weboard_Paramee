fb2519e091cdbca2bea41b9c6b1d3d44e8689840
parameesriphueak <s6603052421038@email.kmutnb.ac.th>
2024-02-07 16:03:53 +0700

    1

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webboard Paramee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
 
     <h1 style="text-align: center;">Webboard Paramee</h1><hr>
-   <form action="">
+   <form>
         หมวดหมู่ :
         <select>
             <option value="all">--ทั้งหมด--</option>
@@ -21,8 +23,8 @@
                 echo"<a href=login.php style = 'float: right';>เข้าสู่ระบบ</a>";
             }else{
                 echo"<div style = 'float: right'>
-                        ผู้ใช้งานระบบ : $_SESSION[username]&nbsp&nbsp
-                        <a href = logout.php>ออกจากระบบ</a>
+                        ผู้ใช้งานระบบ : $_SESSION[username]&nbsp&nbsp;
+                        <a href = logout.php  style = 'float: right';>ออกจากระบบ</a>
                     </div><br>      
                     <a href = nexpost.php>สร้างกระทู้ใหม่</a>";
             }
